974576d8c8df21dc3587737e3d3240b1e0a32764
Randy <anakkantorku@gmail.com>
2025-04-03 15:59:32 +0700

    Step 3 : Create Handle Post, Create File urls.py and Create Url Dispatcher

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 # Create your views here.
@@ -6,4 +6,9 @@ class RegisterView(View):
     def get(self, request):
         return render(request, "register.html")
 
- 
+    def post(request):
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        return redirect('register')
