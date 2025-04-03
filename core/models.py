25188e58d276713e8de69238502e33996c402a92
Randy <anakkantorku@gmail.com>
2025-04-03 16:22:16 +0700

    Step 6 : Create handle Post(LoginView), Create Loop Message, Import User and Create variable actor in /core/models.py

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from .utils import generate_id
+from django.contrib.auth.models import User
 
 class BaseModel(models.Model):
     id = models.CharField(primary_key=True, default=generate_id, editable=False, max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    actor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         abstract = True
